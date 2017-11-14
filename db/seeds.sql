e9d0562b1ddf873709d120e41809d6cb789c98e8
jwilson99 <juliewilsongt@gmail.com>
2017-11-14 18:24:43 -0500

    page displays now using handlebars partials.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Black Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
