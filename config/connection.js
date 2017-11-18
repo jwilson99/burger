463aa6537a43eaad7dbcdbe8fe094ec3b580f47d
jwilson99 <juliewilsongt@gmail.com>
2017-11-18 09:17:12 -0500

    troubleshooting JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // requires mySQL connection
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection( process.env.JAWDB_URL || {
     port: 3306,
     host: "localhost",
     user: "root1",
